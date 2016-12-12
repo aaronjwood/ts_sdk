c9c3bf23b8ad5bc5769f4534ef8439027ae24414
Gary Grebus <gary.grebus@verizon.com>
2016-12-12 12:13:41 -0500

    NPDOTF-51: Fix doxygen markup in cloud_comm.h.

--- include/cloud_comm/cloud_comm.h
+++ include/cloud_comm/cloud_comm.h
@@ -20,9 +20,9 @@
  * used to report the type of the message received.
  * 
  * The API deals with 3 types of messages:\n
- * 	Update   : Application specific byte arrays from the cloud\n
- *      Status   : Application specific byte arrays to the cloud\n
- * 	Command  : OTT protocol defined messages from the cloud
+ *     - Update   : Application specific byte arrays from the cloud\n
+ *     - Status   : Application specific byte arrays to the cloud\n
+ *     - Command  : OTT protocol defined messages from the cloud
  *
  * The state of the secure channel and OTT protocol is maintained automatically
  * based on the API functions that are called.
@@ -119,20 +119,20 @@ typedef void (*cc_callback_rtn)(const cc_buffer_desc *buf, cc_event event);
 /**
  * \brief
  * Initialize the cloud communication API.
+ * 
+ * \param[in] d_ID     : Pointer to a 16 byte device ID.
+ * \param[in] d_sec_sz : Size of the device secret in bytes.
+ * \param[in] d_sec    : Pointer to the buffer holding the device secret.
+ *
+ * \returns
+ *	True  : Initialization was successful.\n
+ *	False : Initialization failed.
  *
  * This will in turn initialize any protocol specific modules, related
  * hardware etc. It also sets the device ID and device secret. These two are
  * used to authenticate the device with the cloud service and are unlikely
  * to change during the lifetime of the device.
  *
- * \params
- * 	@param[in] d_ID     : Pointer to a 16 byte device ID.
- * 	@param[in] d_sec_sz : Size of the device secret in bytes.
- * 	@param[in] d_sec    : Pointer to the buffer holding the device secret.
- *
- * \returns
- *	True  : Initialization was successful.\n
- *	False : Initialization failed.
  */
 bool cc_init(const uint8_t *d_ID, uint16_t d_sec_sz, const uint8_t *d_sec);
 
@@ -140,17 +140,16 @@ bool cc_init(const uint8_t *d_ID, uint16_t d_sec_sz, const uint8_t *d_sec);
  * \brief
  * Set the host name and host port to communicate with.
  *
- * This must be called at least once before attempting to send messages. The
- * lengths of the host name and host port strings are governed by MAX_HOST_LEN
- * and MAX_PORT_LEN, respectively.
- *
- * \params
- * 	@param[in] host : A NULL terminated string specifying the host name.
- * 	@param[in] port : A NULL terminated string specifying the host port.
+ * \param[in] host : A NULL terminated string specifying the host name.
+ * \param[in] port : A NULL terminated string specifying the host port.
  *
  * \returns
  *	True  : Host name and port were set properly.\n
  *	False : Failed to set the host name / host port.
+ *
+ * This must be called at least once before attempting to send messages. The
+ * lengths of the host name and host port strings are governed by MAX_HOST_LEN
+ * and MAX_PORT_LEN, respectively.
  */
 bool cc_set_remote_host(const char *host, const char *port);
 
@@ -159,26 +158,23 @@ bool cc_set_remote_host(const char *host, const char *port);
  * Get a pointer to the first byte of the send buffer from the buffer
  * descriptor.
  *
- * The data to be sent should be written into this buffer before calling
- * cc_send_bytes_to_cloud().
- *
- * \params
- * 	@param[in] buf : A cloud communication buffer descriptor.
+ * \param[in] buf : A cloud communication buffer descriptor.
  *
  * \returns
  * 	Pointer to the send buffer.
+ *
+ * The data to be sent should be written into this buffer before calling
+ * cc_send_bytes_to_cloud().
+ *
  */
 uint8_t *cc_get_send_buffer_ptr(cc_buffer_desc *buf);
 
 /**
  * \brief
  * Get a pointer to the first byte of the receive buffer from the buffer
- * descriptor.
+ * descriptor.  Any received data can be read through this buffer.
  *
- * Any received data can be read through this buffer.
- *
- * \params
- * 	@param[in] buf : A cloud communication buffer descriptor.
+ * \param[in] buf : A cloud communication buffer descriptor.
  *
  * \returns
  * 	Pointer to the receive buffer.
@@ -189,14 +185,15 @@ const uint8_t *cc_get_recv_buffer_ptr(const cc_buffer_desc *buf);
  * \brief
  * Get the value of the sleep interval (in seconds) from the received message.
  *
- * Attempting to retrieve the value from a message that does not contain the
- * sleep interval is unsupported.
- *
- * \params
- * 	@param[in] buf : A cloud communication buffer descriptor.
+ * \param[in] buf : A cloud communication buffer descriptor.
  *
  * \returns
  * 	Value of the sleep interval in seconds.
+ *
+ * \note
+ * Attempting to retrieve the value from a message that does not contain the
+ * sleep interval is unsupported.
+ *
  */
 uint32_t cc_get_sleep_interval(const cc_buffer_desc *buf);
 
@@ -204,8 +201,7 @@ uint32_t cc_get_sleep_interval(const cc_buffer_desc *buf);
  * \brief
  * Retrieve the length of the last message received.
  *
- * \params
- * 	@param[in] buf : A cloud communication buffer descriptor.
+ * \param[in] buf : A cloud communication buffer descriptor.
  *
  * \returns
  * 	Number of bytes of data present in the receive buffer.
@@ -216,22 +212,21 @@ cc_data_sz cc_get_receive_data_len(const cc_buffer_desc *buf);
  * \brief
  * Send bytes to the cloud.
  * 
- * The data is wrapped in protocol headers and sent over a secure channel.
- * A send is said to be active when it is waiting for a response from the
- * cloud services. Only one send can be active at a time.
- *
- * \params
- * 	@param[in] buf : Pointer to the cloud communication buffer descriptor
- * 	       containing the data to be sent.
- * 	@param[in] sz : Size of the data in bytes.
- * 	@param[in] cb : Pointer to the callback routine that will be invoked
- *             with the status of the send.
+ * \param[in] buf : Pointer to the cloud communication buffer descriptor
+ *                  containing the data to be sent.
+ * \param[in] sz : Size of the data in bytes.
+ * \param[in] cb : Pointer to the callback routine that will be invoked
+ *                 with the status of the send.
  *
  * \returns
  * 	CC_SEND_FAILED  : Failed to send the message.\n
  * 	CC_SEND_BUSY    : A send is in progress. \n
  * 	CC_SEND_SUCCESS : Message was sent, waiting for a response from the
- *                        cloud.\n
+ *                        cloud.
+ *
+ * The data is wrapped in protocol headers and sent over a secure channel.
+ * A send is said to be active when it is waiting for a response from the
+ * cloud services. Only one send can be active at a time.
  */
 cc_send_result cc_send_bytes_to_cloud(const cc_buffer_desc *buf, cc_data_sz sz,
 		cc_callback_rtn cb);
@@ -256,20 +251,19 @@ cc_send_result cc_resend_init_config(cc_callback_rtn cb);
  * \brief
  * Initiate a receive of bytes from the cloud.
  *
- * Only one receive can be scheduled at a time. A receive must be scheduled
- * once the API has been initialized.  Scheduling the receive ensures there
- * is a place to receive a response and the appropriate callbacks are invoked.
- *
- * \params
- * 	@param[in] buf  : Pointer to the cloud communication buffer descriptor
- * 	       that will hold the data to be received.
- * 	@param[in] cb   : Pointer to the callback that will be invoked when a
- * 	       complete message is received.
+ * \param[in] buf  : Pointer to the cloud communication buffer descriptor
+ *                   that will hold the data to be received.
+ * \param[in] cb   : Pointer to the callback that will be invoked when a
+ *                   complete message is received.
  *
  * \returns
  * 	CC_RECV_FAILED  : Failed to schedule a receive.
  * 	CC_RECV_BUSY    : Can't initiate a receive. One is already in progress.
  * 	CC_RECV_SUCCESS : Successfully initiated a receive.
+ *
+ * Only one receive can be scheduled at a time. A receive must be scheduled
+ * once the API has been initialized.  Scheduling the receive ensures there
+ * is a place to receive a response and the appropriate callbacks are invoked.
  */
 cc_recv_result cc_recv_bytes_from_cloud(cc_buffer_desc *buf, cc_callback_rtn cb);
 
@@ -277,27 +271,25 @@ cc_recv_result cc_recv_bytes_from_cloud(cc_buffer_desc *buf, cc_callback_rtn cb)
  * \brief
  * Call this function periodically to advance the time-based activities.
  * 
- * This function performs time based activities related to managing the
- * the connection to the cloud and the transactions on it. The CC
- * API does not have an internal timer to work with. Instead, this function
- * performs any pending activities and indicates when it next needs to be called
- * to service a time based event.
- *
- * \params
- * 	@param[in] cur_ts : The current timestamp representing the system tick
- *                          time in ms.
+ * \param[in] cur_ts : The current timestamp representing the system tick
+ *                     time in ms.
  *
  * \returns
  * 	-1             : No time based calls are currently needed.\n
  * 	Positive value : Number of milliseconds after which this function should
  * 	                 be called again.
+ *
+ * This function performs time based activities related to managing the
+ * the connection to the cloud and the transactions on it. The CC
+ * API does not have an internal timer to work with. Instead, this function
+ * performs any pending activities and indicates when it next needs to be called
+ * to service a time based event.
  */
 int32_t cc_service_send_receive(uint32_t cur_ts);
 
 /**
  * \brief
  * Acknowledge the last message received from the cloud services.
- *
  * The actual acknowledgement is transmitted in the next send or as part of
  * time based servicing.
  */
@@ -306,7 +298,6 @@ void cc_ack_bytes(void);
 /**
  * \brief
  * Reject the last message received from the cloud services.
- *
  * Call this when there is an error processing the message. This message is
  * sent immediately to the cloud services.
  */
@@ -317,10 +308,9 @@ void cc_nak_bytes(void);
  * Debug helper function to print the string representation of the contents of
  * the message.
  *
- * \params
- * 	@param[in] buf : Pointer to the cloud communication buffer descriptor
- * 	            containing the received message.
- * 	@param[in] tab_level : Tab level at which the representation should be
+ * \param[in] buf : Pointer to the cloud communication buffer descriptor
+ *                  containing the received message.
+ * \param[in] tab_level : Tab level at which the representation should be
  *                  printed
  */
 void cc_interpret_msg(const cc_buffer_desc *buf, uint8_t tab_level);
