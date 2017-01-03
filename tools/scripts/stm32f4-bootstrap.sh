513570e11a0c717041fbbeb65511430c41a63da4
Gary Grebus <gary.grebus@verizon.com>
2017-01-03 17:55:25 -0500

    NPDOTF-58: Fix STM32F4 toolchain setup to run w/o access to our internal mirror.
    Add support for setting up the STM32F4 buld environment without access to
    the internal Verizon mirror.  Specifying a username parameter retains the old
    behavior and assumes you have SSH access to the internal mirror as that user.
    
    Otherwise, the toolchain file is downloaded from a public repository.
    The .zip file for the STM32CubeF4 library must also be manually
    pre-downloaded since you have to be logged into the STMicro support site to
    access it.
    
    Moved the openocd config files that were previously downloaded from the mirror
    so they now reside in this repo.
    
    Changed common.mk to use whatever version of STM32CubeF4 is in the tools
    directory, rather than being hardwired.

--- tools/scripts/stm32f4-bootstrap.sh
+++ tools/scripts/stm32f4-bootstrap.sh
@@ -1,29 +1,49 @@
 #!/bin/bash
-# Copyright(C) 2016 Verizon. All rights reserved
+# Copyright(C) 2016,2017 Verizon. All rights reserved
 # Script to set up tools needed for building the firmware for the STM32F4
 
-# Name of the toolkits from mirror server
+# Toolkit files on the internal mirror server
 TOOLCHAIN_NAME="gcc-arm-none-eabi-5_4-2016q2-20160622-mac.tar.bz2"
 STM32F4_CUBE_LIB="stm32cubef4-v1.13.0.zip"
-OPENOCD_CONFIG="openocdcfg-stm32f4.zip"
 
 ST_SCRIPT="tools/scripts/stm32f4-bootstrap.sh"
 TOOLS_ROOT="$PWD/tools/installed/stm32f4"
 STM32F4_CFG_SYS="/usr/local/share/openocd/scripts/target/stm32f4x.cfg"
 
-# Mirror server where toolkit resides
-ST_TOOLS_MIRROR="thingservice.verizon.com:/home/vagrant"
+# Internal mirror server where toolkit resides
+INTERNAL_TOOLS_MIRROR="thingservice.verizon.com:/home/vagrant"
 
-if [ $# -ne 1 ]; then
-	echo "Provide username for the thingservice.verizon.com mirror"
-	echo "Usage: $0 <username>"
-	exit 1
-fi
+# External source for GCC toolchain
+GCC_TOOLS_URL="https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q2-update/+download/$TOOLCHAIN_NAME"
 
 
 if ! [ -f $ST_SCRIPT ]; then
 	echo "Script must be run from the root of the git tree."
 	exit 1
+fi
+
+# Invoke with <username> parameter to install from the Verizon internal mirror.
+USERNAME="$1"
+if [ -z "$USERNAME" ]; then
+	cat <<-'EOF'
+	To setup the STM32F4 build environment, you must first download the
+	STM32CubeF4 developement software from the STMicroelectronics site:
+
+	http://www.st.com/content/st_com/en/products/embedded-software/mcus-embedded-software/stm32-embedded-software/stm32cube-embedded-software/stm32cubef4.html
+
+	To do this, you must be logged in with your free myST account.
+
+	EOF
+	read -p "Path to downloaded STM32CubeF4 zip file (<return> to exit): " \
+		answer
+	if [ -z "$answer" ]; then
+		exit 0
+	fi
+	STM32F4_CUBE_LIB=$(printf %q $answer)
+	if ! [ -f "$STM32F4_CUBE_LIB" ]; then
+		echo "File: $STM32F4_CUBE_LIB not found. Exiting.."
+		exit 1
+	fi
 fi
 
 # See if openocd is installed, first check if brew is installed
@@ -33,8 +53,9 @@ OPENOCD_INSTALL=$(which openocd)
 if [ -z "$OPENOCD_INSTALL" ]; then
 	echo "[Installing openocd]"
 	if [ -z "$BREW_INSTALL" ]; then
-		echo "Install homebrew first from http://brew.sh/"
-		echo "Installation not successful..exiting"
+		echo "Installation of openocd is done using homebrew."
+		echo "Please install it first from http://brew.sh/."
+		echo "Exiting.."
 		exit 1
 	fi
 	brew install --HEAD openocd --enable_stlink --with-libusb
@@ -44,13 +65,17 @@ if [ -z "$OPENOCD_INSTALL" ]; then
 		echo "brew failed to install openocd..exiting"
 		exit 1
 	fi
-	echo "[Installed openocd at /user/local/bin]"
 	if ! [ -f $STM32F4_CFG_SYS ]; then
 		echo "OpenOCD target configuration file does not exist."
 		exit 1
 	fi
+	echo "[Installed openocd at /user/local/bin]"
 	echo "[PATH environment variable should append /usr/local/bin]"
+	OPENOCD_INSTALL=/usr/local/bin/openocd
 fi
+OPENOCD_VERSION=$($OPENOCD_INSTALL -v 2>&1 | head -n 1)
+echo
+echo "[Installed openocd version is: $OPENOCD_VERSION]"
 
 if [ -d $TOOLS_ROOT ]; then
 	echo "stm32f4 tools are already installed for this repo at:"
@@ -70,22 +95,28 @@ echo "[Creating $TOOLS_ROOT directory]"
 mkdir -p $TOOLS_ROOT
 
 echo "[Installing toolchain....]"
-	scp $1@$ST_TOOLS_MIRROR/$TOOLCHAIN_NAME .
+	if [ -z "$USERNAME" ]; then
+		wget -O $TOOLCHAIN_NAME $GCC_TOOLS_URL
+	else
+		scp ${USERNAME}@${INTERNAL_TOOLS_MIRROR}/$TOOLCHAIN_NAME .
+	fi
 	tar -C $TOOLS_ROOT/ -x -j -f $TOOLCHAIN_NAME
 	rm -f $TOOLCHAIN_NAME
 echo "[Toolchain installed at $TOOLS_ROOT]"
 
 echo "[Installing openocd configs]"
-	scp $1@$ST_TOOLS_MIRROR/$OPENOCD_CONFIG .
-	unzip -d $TOOLS_ROOT/ $OPENOCD_CONFIG
-	rm -f $OPENOCD_CONFIG
+cp -a $TOOLS_ROOT/../../openocd-configs/stm32f4 $TOOLS_ROOT/openocdcfg-stm32f4
 echo "[Installed openocd configs at $TOOLS_ROOT]"
 
-echo "[Installing stm32cubef4-v1.13.0 library....]"
-	scp $1@$ST_TOOLS_MIRROR/$STM32F4_CUBE_LIB .
-	unzip -d $TOOLS_ROOT/ $STM32F4_CUBE_LIB
-	rm -f $STM32F4_CUBE_LIB
-echo "[library installed at $TOOLS_ROOT]"
+echo "[Installing STM32CubeF4 library....]"
+if [ -z "$USERNAME" ]; then
+	cp $STM32F4_CUBE_LIB .
+else
+	scp ${USERNAME}@${INTERNAL_TOOLS_MIRROR}/$STM32F4_CUBE_LIB .
+fi
+unzip -q -d $TOOLS_ROOT/ $STM32F4_CUBE_LIB
+rm -f ./$(basename $STM32F4_CUBE_LIB)
+echo "[Library installed at $TOOLS_ROOT]"
 
 DOXYGEN_INSTALL=$(which doxygen)
 if [ -z "$DOXYGEN_INSTALL" ]; then
