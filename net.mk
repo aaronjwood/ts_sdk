39dfe4ef88606b9daab1a87fc12f2c73f0be67c3
Gary Grebus <gary.grebus@verizon.com>
2017-01-25 11:48:07 -0500

    Fix reference to file that was renamed.

--- net.mk
+++ net.mk
@@ -1,11 +1,11 @@
-# Copyright(C) 2016 Verizon. All rights reserved.
+# Copyright(C) 2016, 2017 Verizon. All rights reserved.
 
 # Makefile variables to select the necessary files depending on which networking
 # module is being built.
 
 ifeq ($(NET_TYPE),at)
-# Build with networking via LTE modem and AT commands.
-CORELIB_SRC += net_at.c $(MODEM_SRC)
+# Build with networking via LTE modem and AT commands to support mbedtls.
+CORELIB_SRC += net_mbedtls_at.c $(MODEM_SRC)
 
 INC += -I $(PROJ_ROOT)/include/network/at
 INC += -I $(PROJ_ROOT)/include/network
