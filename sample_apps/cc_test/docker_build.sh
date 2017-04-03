70016176272917d4ab32952ee9ff0c64098d7523
Dipen Patel <dipen.patel@one.verizon.com>
2017-04-03 19:40:00 -0400

    changing to sh from bash
    Signed-off-by: Dipen Patel <dipen.patel@one.verizon.com>

--- sample_apps/cc_test/docker_build.sh
+++ sample_apps/cc_test/docker_build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # Copyright(C) 2016,2017 Verizon. All rights reserved
 TOOLS_ROOT="$PWD/../../tools/docker/"
 docker build -t stm32f429_buildenv -f $TOOLS_ROOT/Dockerfile /Users/v659353/Documents/ott_project/ott_firmware/
