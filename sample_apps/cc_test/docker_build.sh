c5f7540096bff38a08cd4161d46b42bf1b060695
Dipen Patel <dipen.patel@one.verizon.com>
2017-04-03 14:44:23 -0400

    Adding docker run command
    Signed-off-by: Dipen Patel <dipen.patel@one.verizon.com>

--- sample_apps/cc_test/docker_build.sh
+++ sample_apps/cc_test/docker_build.sh
@@ -3,3 +3,4 @@
 TOOLS_ROOT="$PWD/../../tools/docker/"
 docker build -t stm32f429_buildenv -f $TOOLS_ROOT/Dockerfile /Users/v659353/Documents/ott_project/ott_firmware/
 docker build -t ott_firmware -f ./Dockerfile /Users/v659353/Documents/ott_project/ott_firmware/
+docker run --rm -v $PWD/build:/build ott_firmware
