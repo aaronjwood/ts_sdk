c2979da1432863f3d867ddfdce47721fe7c27f1e
Dipen Patel <dipen.patel@one.verizon.com>
2017-04-24 16:34:25 -0400

    Renaming timer_<build_mcu> files to timer_interface
    Signed-off-by: Dipen Patel <dipen.patel@one.verizon.com>

--- platform/inc/timer_interface.h
+++ platform/inc/timer_interface.h
@@ -1,7 +1,7 @@
 /**
- * \file timer_stm32f4.h
+ * \file timer_interface.h
  * \copyright Copyright (c) 2017 Verizon. All rights reserved.
- * \brief Defines the generic interface for the stm32f4 family timer peripheral.
+ * \brief Defines the generic timer interface timer peripheral.
  * \details Timers defined and implemented here will be used only for single
  * consumer, for example: SDK AT layer uses timer 2 for stm32f429zi nucleo
  * platform which makes it not available to any other modules inside or outside
@@ -9,14 +9,14 @@
  * instance picking timer from \ref timer_id_t enum
  */
 
-#ifndef TIMER_STM32F4_INTERFACE_H
-#define TIMER_STM32F4_INTERFACE_H
+#ifndef TIMER_INTERFACE_H
+#define TIMER_INTERFACE_H
 
 #include "timer_hal.h"
 
 /**
- * \brief Type that defines timers ids for stm32f4 family, There are total 14
- * timers available for stm32f429zi chipset
+ * \brief Type that defines timers ids, can be modified this enum if timers
+ * for perticular chipset exceeds below limit.
  */
 typedef enum timer_id {
 	TIMER1,
@@ -37,9 +37,11 @@ typedef enum timer_id {
 } timer_id_t;
 
 /**
- * \brief Retrieve the interface of the stm32f4 timer.
+ * \brief Retrieve the interface of timer which later can be used for performing
+ * various timer hal APIs.
  * \param[in] timer id
- * \returns Pointer to timer tim's interface or NULL if fails
+ * \returns Pointer to timer tim's interface or NULL if fails or timer instance
+ * is not implemented.
  */
 const timer_interface_t *timer_get_interface(timer_id_t tim);
 
--- platform/platform.mk
+++ platform/platform.mk
@@ -1,8 +1,6 @@
 # Copyright(C) 2017 Verizon. All rights reserved.
 
 INC += -I $(PLATFORM_HAL_ROOT)/inc
-INC += -I $(PLATFORM_HAL_ROOT)/inc/$(BUILD_TARGET)
-INC += -I $(PLATFORM_HAL_ROOT)/inc/$(BUILD_TARGET)/$(BUILD_MCU)
 INC += -I $(PLATFORM_HAL_ROOT)/sw
 INC += -I $(PLATFORM_HAL_ROOT)/sw/$(BUILD_TARGET)
 INC += -I $(PLATFORM_HAL_ROOT)/sw/$(BUILD_TARGET)/$(BUILD_MCU)
@@ -12,7 +10,7 @@ INC += -I $(PLATFORM_HAL_ROOT)/sw/$(BUILD_TARGET)/$(BUILD_MCU)/$(BUILD_BOARD)
 # This includes the standard per-platform device drivers but not any
 # toolchain or externally supplied files that may need to be built with
 # different compiler options.
-PLATFORM_TIMER_HAL_SRC = timer_hal.c timer_$(BUILD_MCU).c
+PLATFORM_TIMER_HAL_SRC = timer_hal.c timer_interface.c
 PLATFORM_HAL_SRC = dbg_$(BUILD_TARGET).c uart_$(BUILD_TARGET).c
 PLATFORM_HAL_SRC += sys_$(BUILD_TARGET).c gpio_$(BUILD_TARGET).c
 PLATFORM_HAL_SRC += i2c_$(BUILD_TARGET).c
