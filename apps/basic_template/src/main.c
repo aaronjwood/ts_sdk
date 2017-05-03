189289d6a614353c5055f5a6f131e3ca2803a940
Nilangshu <nilangshu.bidyanta@one.verizon.com>
2017-05-03 15:19:32 -0400

    Renamed sample_apps to apps and updated all references;

--- apps/basic_template/src/main.c
+++ apps/basic_template/src/main.c
@@ -1,6 +1,6 @@
 /* Copyright(C) 2017 Verizon. All rights reserved. */
 
-/* For an example showing the SDK usage, please see ts_sdk/sample_apps/cc_test */
+/* For an example showing the SDK usage, please see ts_sdk/apps/cc_test */
 #include "globals.h"
 
 int main(int argc, char* argv[])
