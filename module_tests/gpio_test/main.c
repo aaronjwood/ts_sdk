5c1a1e65b0ce88331f5997668eb69e240864153c
Pushpendra Singh <pushpendra.singh@one.verizon.com>
2017-06-06 13:37:17 -0400

    Porting changes for beduin board

--- module_tests/gpio_test/main.c
+++ module_tests/gpio_test/main.c
@@ -8,23 +8,32 @@
  */
 #include "sys.h"
 #include "gpio_hal.h"
+#include "dbg.h"
+#include "uart_hal.h"
 
 int main()
 {
-	pin_name_t pin_name = PB7;
+	pin_name_t pin_name = PC13;
 	gpio_config_t my_led;
 	sys_init();
+	dbg_module_init();
+	dbg_printf("\r Welcome to Beduin_v2:\r\n");
+
+        dbg_printf("Init LEDS\n\r");
 	my_led.dir = OUTPUT;
 	my_led.pull_mode = PP_PULL_UP;
 	my_led.speed = SPEED_LOW;
 	gpio_init(pin_name, &my_led);
 
+        dbg_printf("set leds in default state\n\r");
+        /* set onboard LEDS */
 	while (1) {
+        	dbg_printf("set leds in OFF state\n\r");
 		gpio_write(pin_name, PIN_LOW);
-		sys_delay(500);
+		sys_delay(1500);
+        	dbg_printf("set leds in ON state\n\r");
 		gpio_write(pin_name, PIN_HIGH);
-		sys_delay(500);
+		sys_delay(1500);
 	}
 	return 0;
 }
-
