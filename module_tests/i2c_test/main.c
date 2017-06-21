24a006f15fc004ab4162fec1118dfce93ef8ecc0
Ramesh Kodam <ramesh.kodam@one.verizon.com>
2017-06-21 15:17:50 -0400

    Validation of I2C read & write APIs based on board type

--- module_tests/i2c_test/main.c
+++ module_tests/i2c_test/main.c
@@ -2,20 +2,48 @@
 
 #include "sys.h"
 #include "dbg.h"
-#include "sensor_i2c_interface.h"
+#include "i2c_hal.h"
+#include "module_config.h"
+
+/* Exit On Error (EOE) macro */
+#define EOE(func) \
+	do { \
+		if (!(func)) \
+			return false; \
+	} while (0)
+
+struct array_t {
+	uint8_t sz;      /* Number of bytes contained in the data buffer */
+	uint8_t *bytes;  /* Pointer to the data buffer */
+};
 
 int main()
 {
 	sys_init();
-	array_t data;
 	dbg_module_init();
+	struct array_t data;
+	uint8_t value = 0x27;
+	i2c_addr_t i2c_dest_addr;
+	uint8_t a[CALIB_SZ] = {0};
+	data.bytes = a;
 	dbg_printf("Begin:\n");
-	
-	dbg_printf("Initializing the sensors\n");
-	ASSERT(si_i2c_init());
-	
-	dbg_printf("Reading sensor data\n");
-	ASSERT(si_i2c_read_data(&data));
 
+	periph_t i2c_handle =  i2c_init(I2C_SCL, I2C_SDA);
+	ASSERT(!(i2c_handle == NO_PERIPH));
+	i2c_dest_addr.slave = SLAVE_ADDR;
+	i2c_dest_addr.reg = SLAVE_ID;
+	EOE(i2c_read(i2c_handle, i2c_dest_addr, 1, data.bytes));
+	if (*(data.bytes) == SLAVE_IDENTIFIER)
+		dbg_printf("Sensor identified\n");
+	else
+		dbg_printf("Wrong Sensor !!!\n");
+
+	i2c_dest_addr.reg = CTRL_REG ;
+	printf("writing value  of 0x%2x in to the register\n", value);
+	EOE(i2c_write(i2c_handle, i2c_dest_addr, 1 , &value));
+
+	printf("Reading value from the register\n");
+	EOE(i2c_read(i2c_handle, i2c_dest_addr, 1, data.bytes));
+	dbg_printf("value is 0x%2x\n", *(data.bytes));
 	return 0;
 }
--- module_tests/inc/module_config.h
+++ module_tests/inc/module_config.h
@@ -10,14 +10,14 @@
 /* GPIO pin connected to an LED to verify the GPIO test */
 #define GPIO_LED_PIN		PC13
 
-/* BME280 sensor registers and addresses */
+/* BME280 sensor registers and values */
 #define I2C_SCL                 PB6   /* I2C Serial clock pin */
 #define I2C_SDA                 PB7   /* I2C Serical data pin */
 #define SLAVE_ADDR              0x77  /* I2C device address */
 #define SLAVE_ID                0xD0  /* Device identfication register */
 #define SLAVE_IDENTIFIER        0x60  /* Device identification value */
 #define CTRL_REG                0xF4  /* Sensor control register */
-#define CALIB_SZ                26    /* calibration table length */ 
+#define CALIB_SZ                26    /* calibration table length */
 
 /* Other module conifgs */
 
@@ -26,14 +26,14 @@
 /* GPIO pin connected to an LED to verify the GPIO test */
 #define GPIO_LED_PIN		PB14
 
-/* BME280 sensor registers and addresses */
-#define I2C_SCL                 PB8  /* I2C Serial clock pin */
-#define I2C_SDA                 PB9  /* I2C Serial data pin */
-#define SLAVE_ADDR              0x5f /* I2C Device address  */
-#define SLAVE_ID                0x0f /* Device identfication register */
-#define SLAVE_IDENTIFIER        0xBC /* Device identfication value */
-#define CTRL_REG                0x20 /* Sensor control register */
-#define CALIB_SZ                16   /*calibration table length */  
+/* xnucleo-iks01a1 hts21 sensor registers and values */
+#define I2C_SCL                 PB8   /* I2C Serial clock pin */
+#define I2C_SDA                 PB9   /* I2C Serial data pin */
+#define SLAVE_ADDR              0x5f  /* I2C Device address  */
+#define SLAVE_ID                0x0f  /* Device identfication register */
+#define SLAVE_IDENTIFIER        0xBC  /* Device identfication value */
+#define CTRL_REG                0x20  /* Sensor control register */
+#define CALIB_SZ                16    /*calibration table length */
 
 /* Other module conifgs */
 
