3099151bea75a2ea4be5c3c62779625708883b5d
Dipen Patel <dipen.patel@one.verizon.com>
2017-07-25 15:19:32 -0400

    removed if conditions
    Signed-off-by: Dipen Patel <dipen.patel@one.verizon.com>

--- apps/device_info/include/app_info.h
+++ apps/device_info/include/app_info.h
@@ -8,30 +8,4 @@
 #define PROF_NAME       "diagnostic_info"
 #define PROF_ID         "456"
 
-#if 0
-typedef struct char_name_t
-{
-    const char *char_name;
-    const char *para_type;
-    const char *access_level;
-    char current_value[10];
-    char last_value[10];
-    const char *value_unit;
-    const char **provided_values;
-    int num_of_provided_values;
-    const char *additional_info;
-} char_t;
-
-#define SENSOR_PROFILE_ID_SIZE  (16)
-
-typedef struct
-{
-    const char *profile;
-    char id[SENSOR_PROFILE_ID_SIZE];
-    char_t *char_name;   /*pointer to the characteristics array*/
-    uint32_t chr_number;
-} sensor_profile_data_t;
-
-#endif
-
 #endif
